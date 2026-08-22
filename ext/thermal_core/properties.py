a1ba18176615322627e309e17c13acc521e59ee0
lorenzozanizz <lorenzo1.zani@mail.polimi.it>
2026-08-22 22:12:06 +0200

    feature: blender properties import stubs

---
+++ ext/thermal_core/properties.py
@@ -0,0 +1,7 @@
+from bpy.types import PropertyGroup, NodeTree
+from bpy.props import PointerProperty, StringProperty, BoolProperty, IntProperty, CollectionProperty
+
+
+class SceneIgnoreRules(PropertyGroup):
+    """ """
+    pass
---
+++ ext/ui/properties.py
@@ -0,0 +1,10 @@
+from bpy.types import PropertyGroup, NodeTree
+from bpy.props import PointerProperty, StringProperty, BoolProperty, IntProperty, CollectionProperty
+
+class SceneIgnoreRules(PropertyGroup):
+    """ """
+    pass
+
+
+class ThermalProperties(PropertyGroup):
+    pass
