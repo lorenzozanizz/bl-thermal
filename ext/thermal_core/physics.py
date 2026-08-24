ac570b4446fcf8c0b3ea9a907406d4759c533766
lorenzozanizz <lorenzo1.zani@mail.polimi.it>
2026-08-24 12:08:01 +0200

    chore: mainly docs and shaders stubs

---
+++ ext/thermal_core/physics.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# Namespace class
+class PhysicsFunc:
+    """
+
+    """
+
+    @staticmethod
+    def apply_emissivity(l_surface: np.ndarray, eps, l_reflected) -> np.ndarray:
+        """ L_apparent = eps*B(T_surf) + (1-eps)*B(T_reflected). eps and
+        l_reflected broadcast (scalars or per-pixel/per-vertex maps), and B is the
+        radiated Boltzmann thermal energy
+
+        For more resources:
+        https://www.flir.com/discover/professional-tools/how-does-emissivity-affect-thermal-imaging/
+
+        :param l_surface: emitted energy of the surface,
+        :param eps: emissivity of the surface,
+        :param l_reflected: reflected energy
+        """
+        eps = np.asarray(eps, dtype=np.float64)
+        return eps * l_surface + (1.0 - eps) * np.asarray(l_reflected)
+
+    @staticmethod
+    def planck_law(t: np.ndarray) -> np.ndarray:
+        pass
+
+
---
+++ ext/ui/main_panel.py
@@ -0,0 +1,27 @@
+from abc import abstractmethod, ABCMeta
+
+
+class UISection(metaclass=ABCMeta):
+    pass
+
+class BakeSection(UISection):
+    """
+
+    """
+    pass
+
+
+class SpecSection(UISection):
+    """
+
+    """
+    pass
+
+
+class RenderSection(UISection):
+    pass
+
+
+
+class CentralPanel(UISection):
+    pass
